bd06b485be3bec90f326bfbec0adb6dd06b7c18d
Miguel Trejo <adrian@develupme.com>
2018-10-04 01:49:07 -0500

    migrations, controllers & models venta,construidos,arquitectonicos

---
+++ app/Http/Controllers/ArquitectonicoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Arquitectonico;
+use Illuminate\Http\Request;
+
+class ArquitectonicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Arquitectonico  $arquitectonico
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Arquitectonico $arquitectonico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Arquitectonico  $arquitectonico
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Arquitectonico $arquitectonico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Arquitectonico  $arquitectonico
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Arquitectonico $arquitectonico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Arquitectonico  $arquitectonico
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Arquitectonico $arquitectonico)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ConstruidoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Construido;
+use Illuminate\Http\Request;
+
+class ConstruidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Construido  $construido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Construido $construido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Construido  $construido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Construido $construido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Construido  $construido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Construido $construido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Construido  $construido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Construido $construido)
+    {
+        //
+    }
+}
